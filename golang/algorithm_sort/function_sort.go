6a93f85791a4fef16a881a9d16af229e118baf33
aninstein@163.com <13132208596@qq.com>
2020-08-12 23:48:36 +0800

    更新文档之间内容

--- golang/algorithm_sort/function_sort.go
+++ golang/algorithm_sort/function_sort.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
+func main() {
+    data := []int{1, 5, 7, 1, 8, 9, 4, 2, 3, -1, 14, 41, -22}
+    fmt.Println(bubbleSort(data))
+
+}
+
 
 func bubbleSort(data []int) []int{
     if data == nil || len(data) == 0 {
