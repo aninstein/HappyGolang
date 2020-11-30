55811003bc557eeb130d7e60f79558e46e770b81
lichangan@hp <lichangan@163.com>
2020-12-01 02:40:31 +0800

    链表排序

--- golang/leetcode_go/mid_linklist_sort.go
+++ golang/leetcode_go/mid_linklist_sort.go
@@ -10,6 +10,14 @@ type ListNode struct {
     Next *ListNode
 }
 
+/*
+题目：链表插入排序
+题目链接：https://leetcode-cn.com/problems/insertion-sort-list/
+
+输入: 4->2->1->3
+输出: 1->2->3->4
+
+*/
 func insertionSortList(head *ListNode) *ListNode {
 	if head == nil {
 		return nil
@@ -46,6 +54,70 @@ func insertionSortList(head *ListNode) *ListNode {
 	}
 	return newHead
 }
+
+
+/*
+题目：对链表进行排序
+题目链接：https://leetcode-cn.com/problems/sort-list/
+
+输入：head = [-1,5,3,4,0]
+输出：[-1,0,3,4,5]
+
+*/
+func createHeapList(head *ListNode) []*ListNode {
+	if head == nil {
+		return nil
+	}
+	var heapData []*ListNode
+	now := head
+	for now != nil {
+		heapData = insertMinHeap(heapData, now)
+		now = now.Next
+	}
+	return heapData
+}
+
+func insertMinHeap(data []*ListNode, node *ListNode) []*ListNode {
+	if data == nil {
+		return []*ListNode{node}
+	}
+	dataLen := len(data)
+	if dataLen == 1 {
+		data = append(data, node)
+		if data[0].Val > data[1].Val {
+			return []*ListNode{data[0], data[1]}
+		}
+		return data
+	} else if dataLen == 2 {
+		if data[0].Val > node.Val {
+			return []*ListNode{data[0], data[1], node}
+		} else {
+			return []*ListNode{node, data[0], data[1]}
+		}
+	}
+
+	index := dataLen // (dataLen - 1) + 1, +1是因为append了一个
+	data = append(data, node)
+	adjustNodeBottom2Up(data, index)
+	return data
+}
+
+func adjustNodeBottom2Up(data []*ListNode, index int) []*ListNode {
+	parent := int((index - 1) / 2)
+	node := data[index]
+	for parent >= 0 {
+		if data[index].Val > data[parent].Val {
+			break
+		}
+
+		data[parent], data[index] = data[index], data[parent]
+		index = parent
+		parent = int((index - 1) / 2)
+	}
+	data[index] = node
+	return data
+}
+
 
 func createListLink(data []int) *ListNode {
 	dataLen := len(data)
