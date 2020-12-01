6d31e2cc8f907ca511a2a300ec5e824df1d1aaed
aninstein <13132208596@163.com>
2020-12-01 17:36:03 +0800

    链表排序-归并排序

--- golang/leetcode_go/mid_linklist_sort.go
+++ golang/leetcode_go/mid_linklist_sort.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
+	"time"
 )
 
 // Definition for singly-linked list.
@@ -9,6 +12,8 @@ type ListNode struct {
     Val int
     Next *ListNode
 }
+
+var TESTTIME map[int] string
 
 /*
 题目：链表插入排序
@@ -179,6 +184,70 @@ func adjustNodeTop2down(data []*ListNode, index int) []*ListNode {
 	return data
 }
 
+
+/*
+归并排序
+*/
+func merge(head1, head2 *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	temp, temp1, temp2 := dummyHead, head1, head2
+	for temp1 != nil && temp2 != nil {
+		if temp1.Val <= temp2.Val {
+			temp.Next = temp1
+			temp1 = temp1.Next
+		} else {
+			temp.Next = temp2
+			temp2 = temp2.Next
+		}
+		temp = temp.Next
+	}
+	if temp1 != nil {
+		temp.Next = temp1
+	} else if temp2 != nil {
+		temp.Next = temp2
+	}
+	return dummyHead.Next
+}
+
+func mergeSort(head, tail *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	if head.Next == tail {
+		head.Next = nil
+		return head
+	}
+
+	slow, fast := head, head
+	for fast != tail {
+		slow = slow.Next
+		fast = fast.Next
+		if fast != tail {
+			fast = fast.Next
+		}
+	}
+
+	mid := slow
+	return merge(mergeSort(head, mid), mergeSort(mid, tail))
+}
+
+func mergeSortList(head *ListNode) *ListNode {
+	return mergeSort(head, nil)
+}
+
+
+/*
+* 共用方法
+*/
+func createListData(number int) []int {
+	var data []int
+	for i := 0; i < number; i++ {
+		data = append(data, rand.Intn(number))
+	}
+	return data
+}
+
 func createListLink(data []int) *ListNode {
 	dataLen := len(data)
 	if dataLen == 0 {
@@ -200,7 +269,7 @@ func createListLink(data []int) *ListNode {
 
 func printListLink(node *ListNode)  {
 	for true {
-		fmt.Println(node.Val)
+		fmt.Print(" ", node.Val)
 		if node.Next == nil {
 			break
 		}
@@ -208,12 +277,57 @@ func printListLink(node *ListNode)  {
 	}
 }
 
+func checkResult(head, right *ListNode) string {
+	now := head
+	rightNow := right
+	for now != nil {
+		if now.Val != rightNow.Val {
+			return "false"
+		}
+		now = now.Next
+		rightNow = rightNow.Next
+	}
+	return "true"
+}
+
+func sortTestFunc(sortFunc func(head *ListNode) *ListNode, head, right *ListNode, name string) *ListNode {
+	usHead := head
+	fmt.Println("# "+ name +" start >>>>>>>>>>>>>>>>>>")
+	start := time.Now().UnixNano()
+	resData := sortFunc(usHead)
+	total := time.Now().UnixNano() - start
+	TESTTIME[int(total)] = name
+	fmt.Println("# "+ name +" end, total time: ", total)
+	fmt.Println("# "+ name +" sort result is ", checkResult(usHead, right))
+	fmt.Println("# "+ name + " result: ")
+	printListLink(resData)
+	fmt.Println("\n")
+	return resData
+}
+
 func main() {
-	data := []int{1, 5, 8, 5, 4, 2, 3, 4, 7, 9}
-	res := createListLink(data)
+	data := createListData(100)
+	linkList := createListLink(data)
 	fmt.Println("data >>>>>>>>>>>>>>>")
-	printListLink(res)
-	fmt.Println("sort data >>>>>>>>>>>>>")
-	sortRes := heapSortLinkList(res)
-	printListLink(sortRes)
+	printListLink(linkList)
+	TESTTIME = make(map[int] string)
+	retData := sortTestFunc(insertionSortList, linkList, &ListNode{}, "insertionSortList")
+
+	linkList = createListLink(data)
+	sortTestFunc(heapSortLinkList, linkList, retData, "heapSortLinkList")
+
+	linkList = createListLink(data)
+	sortTestFunc(mergeSortList, linkList, retData, "mergeSortList")
+
+	var keyList []int
+	for key := range TESTTIME {
+		keyList = append(keyList, key)
+	}
+	sort.Ints(keyList)
+
+
+	for i:=0; i<len(keyList); i++ {
+		key := keyList[i]
+		fmt.Println("time: ", key, "func name: ", TESTTIME[key])
+	}
 }
